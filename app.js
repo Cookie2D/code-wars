099136dcb01ae866bdb5b9227a203fae178efe47
Ihor Sharov <cookie50ds@gmail.com>
2023-12-13 23:25:36 +0200

    add group by function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const persons = [
+var persons = [
   { name: "Peter", profession: "teacher", age: 20, maritalStatus: "married" },
   { name: "Michael", profession: "teacher", age: 50, maritalStatus: "single" },
   { name: "Peter", profession: "teacher", age: 20, maritalStatus: "married" },
@@ -11,14 +11,17 @@ const persons = [
 function profession(person) {
   return person.profession;
 }
-function superName(person) {
+function name(person) {
   return person.name;
 }
+function age(person) {
+  return person.age;
+}
 function isTeacher(person) {
-  return person.profession === 'teacher';
+  return person.profession === "teacher";
 }
 function isPeter(person) {
-  return person.name === 'Peter';
+  return person.name === "Peter";
 }
 
 function query() {
@@ -31,7 +34,7 @@ function query() {
 
   actions.select = function (callback = null) {
     if (actions.selectCallback !== undefined) {
-      throw new Error('Duplicate SELECT');
+      throw new Error("Duplicate SELECT");
     }
 
     actions.selectCallback = callback;
@@ -40,58 +43,87 @@ function query() {
   };
 
   actions.from = function (objectsArray = null) {
-    if(actions.result !== undefined) {
-      throw new Error('Duplicate FROM');
+    if (actions.result !== undefined) {
+      throw new Error("Duplicate FROM");
     }
-    
+
     actions.result = objectsArray;
 
     return actions;
   };
-  
-  actions.where = function(filter = null) {
-    if(filter) {
-      actions.filterCallbackStack.push(filter)
+
+  actions.where = function (filter = null) {
+    if (filter) {
+      actions.filterCallbackStack.push(filter);
     }
 
     return actions;
-  }
+  };
 
-  actions.groupBy = function(group) {
-    if(actions.groupByCallback !== undefined) {
-      throw new Error('Duplicate GROUP BY');
+  actions.groupBy = function (...groups) {
+    if (actions.groupByCallback !== undefined) {
+      throw new Error("Duplicate GROUP BY");
     }
-    
-    actions.groupByCallback = group;
+
+    actions.groupByCallback = groups;
 
     return actions;
-  }
+  };
 
   actions.execute = function () {
     let res = actions.result || [];
 
     // Execute where =>
     res = res.filter((el) => {
-      return actions.filterCallbackStack.every(callback => callback(el));
-    })
+      return actions.filterCallbackStack.every((callback) => callback(el));
+    });
 
     // Execute groupBy =>
-    const tmp = res.map();
-    console.log(tmp)
+    if (actions.groupByCallback) {
+      res = group(res, actions.groupByCallback);
+    }
 
     // Execute select =>
-    res = res.map((el) => {
-      if (actions.selectCallback) {
-        return actions.selectCallback(el)
-      }
-
-      return el;
-    });
+    if (actions.selectCallback) {
+      res = res.map((el) => {
+        return actions.selectCallback(el);
+      });
+    }
 
     return res;
   };
 
   return actions;
+}
+
+function group(array, callbacks) {
+  // refactore
+  const callbackCopy = [...callbacks];
+  const callback = callbackCopy.shift();
+
+  const groups = array.reduce((acc, el) => {
+    const group = callback(el);
+    if (!acc?.[group]) acc[group] = [];
+    acc[group].push(el);
+
+    return acc;
+  }, {});
+
+  const grouped = Object.entries(groups).reduce((acc, [key, values]) => {
+    let groups = [...values];
+
+    if (callbackCopy.length) {
+      groups = group(groups, callbackCopy);
+    }
+
+    const isDigit = !Number.isNaN(Number(key));
+
+    acc.push([isDigit ? Number(key) : key, [...groups]]); // do it in more smart way!
+
+    return acc;
+  }, []);
+
+  return grouped;
 }
 
 // console.log(
@@ -105,8 +137,8 @@ function query() {
 //     .execute()
 // );
 
-query().select().from(persons).groupBy(profession).execute(); 
-
+const res = query().select().from(persons).groupBy(age).execute();
+console.log(JSON.stringify(res));
 
 // console.log(first)
 
@@ -114,8 +146,9 @@ query().select().from(persons).groupBy(profession).execute();
 // console.log(query().select().from([]).select().execute()) // "Error('Duplicate SELECT')"
 // console.log(query().select().from([]).from([]).execute()); //Error('Duplicate FROM');
 
-
 // const numbers = [1, 2, 3];
 // console.log(query().select().execute(), [])
 // console.log(query().from(numbers).execute(), [1, 2, 3])
 // console.log(query().execute(), [])
+
+module.exports = { query };
---
+++ temp.js
@@ -0,0 +1,24 @@
+[
+  [
+    "teacher",
+    [
+      [
+        "Peter",
+        [
+          { name: "Peter", profession: "teacher", age: 20, maritalStatus: "married" },
+          { name: "Peter", profession: "teacher", age: 20, maritalStatus: "married" },
+        ],
+      ],
+      ["Michael", [{ name: "Michael", profession: "teacher", age: 50, maritalStatus: "single" }]],
+    ],
+  ],
+  [
+    "scientific",
+    [
+      { name: "Anna", profession: "scientific", age: 20, maritalStatus: "married" },
+      { name: "Rose", profession: "scientific", age: 50, maritalStatus: "married" },
+      { name: "Anna", profession: "scientific", age: 20, maritalStatus: "single" },
+    ],
+  ],
+  ["politician", [{ name: "Anna", profession: "politician", age: 50, maritalStatus: "married" }]],
+];
